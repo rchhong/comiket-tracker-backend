21989a2b290bd831817a48069d79691eca0f5e19
Ryan Chhong <ryanchhong@gmail.com>
2025-12-30 20:59:39 -0600

    upserting reservation no longer automatically creates resources

--- internal/controllers/reservations.go
+++ internal/controllers/reservations.go
@@ -1,12 +1,10 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/rchhong/comiket-backend/internal/controllers/utils"
-	"github.com/rchhong/comiket-backend/internal/models"
 	"github.com/rchhong/comiket-backend/internal/service"
 )
 
@@ -34,7 +32,7 @@ func (reservationController ReservationController) getReservationsForUser(r *htt
 	return reservations, http.StatusCreated, nil
 }
 
-func (reservationController ReservationController) upsertReservation(r *http.Request) (any, int, error) {
+func (reservationController ReservationController) createReservation(r *http.Request) (any, int, error) {
 	melonbooksId, parseErr := strconv.ParseInt(r.PathValue("melonbooksId"), 10, 64)
 	if parseErr != nil {
 		return nil, http.StatusBadRequest, parseErr
@@ -45,13 +43,7 @@ func (reservationController ReservationController) upsertReservation(r *http.Req
 		return nil, http.StatusBadRequest, parseErr
 	}
 
-	var user models.User
-	parseErr = json.NewDecoder(r.Body).Decode(&user)
-	if parseErr != nil {
-		return nil, http.StatusBadRequest, parseErr
-	}
-
-	reservation, err := reservationController.reservationService.CreateReservation(r.Context(), int(melonbooksId), discordId, user)
+	reservation, err := reservationController.reservationService.CreateReservation(r.Context(), int(melonbooksId), discordId)
 	if err != nil {
 		return nil, err.Status(), err
 
@@ -82,6 +74,6 @@ func (reservationController ReservationController) deleteReservation(r *http.Req
 
 func (reservationController *ReservationController) RegisterReservationController(mux *http.ServeMux) {
 	utils.RegisterMethodToHTTPServer(mux, http.MethodGet, "/doujins/{melonbooksId}/reservations", reservationController.getReservationsForUser)
-	utils.RegisterMethodToHTTPServer(mux, http.MethodPut, "/doujins/{melonbooksId}/reservations/{discordId}", reservationController.upsertReservation)
+	utils.RegisterMethodToHTTPServer(mux, http.MethodPost, "/doujins/{melonbooksId}/reservations/{discordId}", reservationController.createReservation)
 	utils.RegisterMethodToHTTPServer(mux, http.MethodDelete, "/doujins/{melonbooksId}/reservations/{discordId}", reservationController.deleteReservation)
 }
